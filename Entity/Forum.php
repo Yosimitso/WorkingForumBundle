975ad59bf1514e08e5bbe0d1d50ef83c26ae8992
Yosimitso <charlycarlos@msn.com>
2016-05-26 10:17:28 +0200

    Correction forum management

--- Entity/Forum.php
+++ Entity/Forum.php
@@ -89,7 +89,7 @@ class Forum
     
     public function addSubForum(\Yosimitso\WorkingForumBundle\Entity\Subforum $subforum)
     {
-        $this->subforum[] = $subforum; 
+        $this->subForum[] = $subforum; 
     }
     
      /**
@@ -115,4 +115,24 @@ class Forum
         return $this->slug;
     }
     
+    
+    public function genSlug($name)
+    {
+        $this->slug = $this->clean($name);
+        return $this->slug;
+    }
+    
+           private function clean ($str)
+        {
+	/** Mise en minuscules (chaîne utf-8 !) */
+	$str = mb_strtolower($str, 'utf-8');
+	/** Nettoyage des caractères */
+	mb_regex_encoding('utf-8');
+	$str = trim(preg_replace('/ +/', ' ', mb_ereg_replace('[^a-zA-Z\p{L}]+', ' ', $str)));
+	/** strtr() sait gérer le multibyte */
+	$str = strtr($str, array(
+	' ' => '-', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'a'=>'a', 'a'=>'a', 'a'=>'a', 'ç'=>'c', 'c'=>'c', 'c'=>'c', 'c'=>'c', 'c'=>'c', 'd'=>'d', 'd'=>'d', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'e'=>'e', 'e'=>'e', 'e'=>'e', 'e'=>'e', 'e'=>'e', 'g'=>'g', 'g'=>'g', 'g'=>'g', 'h'=>'h', 'h'=>'h', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'i'=>'i', 'i'=>'i', 'i'=>'i', 'i'=>'i', 'i'=>'i', '?'=>'i', 'j'=>'j', 'k'=>'k', '?'=>'k', 'l'=>'l', 'l'=>'l', 'l'=>'l', '?'=>'l', 'l'=>'l', 'ñ'=>'n', 'n'=>'n', 'n'=>'n', 'n'=>'n', '?'=>'n', '?'=>'n', 'ð'=>'o', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'o'=>'o', 'o'=>'o', 'o'=>'o', 'œ'=>'o', 'ø'=>'o', 'r'=>'r', 'r'=>'r', 's'=>'s', 's'=>'s', 's'=>'s', 'š'=>'s', '?'=>'s', 't'=>'t', 't'=>'t', 't'=>'t', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'u'=>'u', 'u'=>'u', 'u'=>'u', 'u'=>'u', 'u'=>'u', 'u'=>'u', 'w'=>'w', 'ý'=>'y', 'ÿ'=>'y', 'y'=>'y', 'z'=>'z', 'z'=>'z', 'ž'=>'z'
+	));
+	return $str;
+        }
 }
